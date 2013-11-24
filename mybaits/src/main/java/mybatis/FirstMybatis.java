4f99007725371053452849fbadceec3418d09d4a
javiosyc <javiosyc@gmail.com>
2013-11-24 15:54:10 +0800

    test mybatis  1.add mybatis configuration  2.add simple example using mybatis  3.add log4j settings

---
+++ mybaits/src/main/java/mybatis/FirstMybatis.java
@@ -0,0 +1,95 @@
+package mybatis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class FirstMybatis {
+
+	public static void main(String[] args) throws IOException, SQLException {
+
+		String resource = "mybatis/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+
+		final String ENV = "test";
+
+		// 1.using test env SqlSessionFactoryBuilder create SqlSessionFactory
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(
+				inputStream, ENV);
+
+		SqlSession session = factory.openSession();
+
+		System.out.println(session.getConfiguration().getEnvironment().getDataSource().getConnection());
+
+		try {
+
+			// 2.using mapper
+			FirstMybatisMapper mapper = session
+					.getMapper(mybatis.FirstMybatisMapper.class);
+
+			Map<String, Object> param = new HashMap<String, Object>();
+			param.put("id", 1);
+			
+			// 2.1 param
+			Map<String, Object> map = mapper.queryMailSettings(param);
+
+			showMailSetting(map);
+			String sql = map.get("sql").toString();
+
+			param.clear();
+			param.put("sql", sql);
+
+			// 2.2 dynamic sql
+			List<Map<String, Object>> exeuteResult = mapper.executeSQL(param);
+			System.out.println(exeuteResult.get(0).get("id"));
+
+			// 3. using annotation
+			param.clear();
+			param.put("id", 2);
+			Map<String, Object> result = mapper.testAnnotation(param);
+			System.out.println(result.get("id"));
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			session.close();
+		}
+
+	}
+
+	private static void showMailSetting(Map<String, Object> map) {
+		
+		System.out.println(map.get(MailSetting.CC.getDBColumnName()));
+		System.out.println(map.get(MailSetting.BCC.getDBColumnName()));
+		System.out.println(map.get(MailSetting.CONTEXT.getDBColumnName()));
+		System.out.println(map.get(MailSetting.ENV.getDBColumnName()));
+
+	}
+
+	private enum MailSetting {
+		CC("CC","CC"), BCC("BCC","BCC"), CONTEXT("CONTEXT","CONTEXT"),
+		ENV("","ENV"), RECEIVER("email","RECEIVER");
+
+		private String emailTitle;
+		private String dbColumnName;
+		MailSetting(String emailTitle,String dbColumnName) {
+			this.emailTitle = emailTitle;
+			this.dbColumnName = dbColumnName;
+		}
+
+		public String getEmailTitle() {
+			return this.emailTitle;
+		}
+		public String getDBColumnName(){
+			return this.dbColumnName;
+		}
+	}
+}
---
+++ mybaits/src/main/java/mybatis/FirstMybatisMapper.java
@@ -0,0 +1,16 @@
+package mybatis;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface FirstMybatisMapper {
+	
+	public Map<String, Object> queryMailSettings(Map<String,Object> param);
+	
+	public List<Map<String,Object>> executeSQL(Map<String,Object> param);
+	
+	@Select("Select * from user where id = #{id}")
+	public Map<String,Object> testAnnotation(Map<String,Object> param);
+}
